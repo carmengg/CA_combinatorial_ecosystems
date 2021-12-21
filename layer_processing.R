7420177e4e8c1651b4b659e2ab30a046fc07537f
Carmen Galaz-García <33770204+carmengg@users.noreply.github.com>
2021-12-21 12:53:47 -0800

    cleaned and simplified function for aridity layer

--- layer_processing.R
+++ layer_processing.R
@@ -14,13 +14,67 @@
 
 
 #########################################################
-#                 RASTER PREPARATION
+#                 RASTER CROPPING
 #########################################################
 
 # shape needs to be opened via readOGR() in library(rgdal) 
 crop_shape <- function(ref_raster , shape){
   new_shape <- spTransform(shape, crs(ref_raster))
   return(mask(crop(x= ref_raster, y=st_bbox(new_shape)),new_shape))
+}
+
+
+#########################################################
+#           OUTLIER DETECTION & REPLACEMENT
+#########################################################
+
+# returns a raster with same extent as rast,
+# with all values outside (low_end, high_end) converted to some out_value
+# and all other values to NA
+outliers_rast <- function(rast, low_end, high_end){
+  
+  out_value = as.integer(summary(rast)[5]*10)  
+  # create value outside raster range
+  # summary(rast)[5] is the max value on the raster
+  
+  bad_data <- rast
+  bad_data[bad_data<low_end] <- out_value
+  bad_data[bad_data>high_end] <- out_value  
+  bad_data[bad_data!= out_value] <- NA
+  
+  return(bad_data)
+}
+# ----------------------------------------------------
+
+# returns a list of indices of the pixels in rast 
+# with values outside (low_end, high_end) 
+outliers <- function(rast, low_end, high_end){
+  
+  bad_indices <- which((rast[] > high_end) | (rast[] < low_end))
+  
+  return(bad_indices)
+}
+
+# ----------------------------------------------------
+
+# returns a copy of the rast raster 
+# with the outliers replaced by the average of buffer=100000 cells around
+replace_outliers <- function(rast,low_end, high_end){
+  
+  bad_indices <- outliers(rast,low_end, high_end)
+  
+  # remove outliers
+  new_rast <- rast
+  new_rast[new_rast<low_end] <- NA
+  new_rast[new_rast>high_end] <- NA
+  
+  result<- extract(new_rast, 
+                   xyFromCell(new_rast, bad_indices), 
+                   buffer=100000, 
+                   fun=mean,
+                   na.rm=TRUE)
+  new_rast[bad_indices] <- result
+  return(new_rast)
 }
 
 
@@ -79,4 +133,25 @@ biomass_regions <- function(lc_raster){
   return(new_raster)
 }
 
+##################################################################################
+#                       ARIDITY INDEX
+# functions originally coded at aridity_replacing_outliers.Rmd
+# 
 
+#################################################################################
+
+# reclassify values into three groups: 
+# 1 = desert => AI < 0.05
+# 2 = dry => 0.05 <= AI <= 0.65
+# 3 = moist => 0.65 < AI
+# https://www.rdocumentation.org/packages/raster/versions/3.5-2/topics/reclassify
+# min, max, new_value
+aridity_regions <- function(arid_raster){
+  m <- c(0, 500 , 1,  
+         500, 6500, 2,  
+         6500, Inf, 3)
+  thresh <- matrix(m, ncol=3, byrow=TRUE)
+  
+  # double check open/closed intervals
+  return(reclassify(arid_raster*10000,thresh))
+}
